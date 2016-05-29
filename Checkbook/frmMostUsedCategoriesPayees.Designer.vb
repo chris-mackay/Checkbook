0475b77c749bf18e498ef8fa7942d0feba15ea40
Chris Mackay <chkbookapp@gmail.com>
2016-05-29 13:02:07 -0400

    Added frmMostUsedCategoriesPayees and started adding code.

---
+++ Checkbook/frmMostUsedCategoriesPayees.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMostUsedCategoriesPayees
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.dgvMostUsed = New System.Windows.Forms.DataGridView()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.gbFilterOptions = New System.Windows.Forms.GroupBox()
+        Me.cbYear = New System.Windows.Forms.ComboBox()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.cbCategoriesPayees = New System.Windows.Forms.ComboBox()
+        Me.lblFilterCategoriesPayees = New System.Windows.Forms.Label()
+        CType(Me.dgvMostUsed, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbFilterOptions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvMostUsed
+        '
+        Me.dgvMostUsed.AllowUserToAddRows = False
+        Me.dgvMostUsed.AllowUserToDeleteRows = False
+        Me.dgvMostUsed.AllowUserToResizeRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.dgvMostUsed.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvMostUsed.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvMostUsed.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvMostUsed.BackgroundColor = System.Drawing.SystemColors.ControlLightLight
+        Me.dgvMostUsed.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dgvMostUsed.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        Me.dgvMostUsed.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.LightSteelBlue
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgvMostUsed.DefaultCellStyle = DataGridViewCellStyle2
+        Me.dgvMostUsed.GridColor = System.Drawing.Color.LightGray
+        Me.dgvMostUsed.Location = New System.Drawing.Point(12, 97)
+        Me.dgvMostUsed.MultiSelect = False
+        Me.dgvMostUsed.Name = "dgvMostUsed"
+        Me.dgvMostUsed.RowHeadersVisible = False
+        Me.dgvMostUsed.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.dgvMostUsed.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvMostUsed.Size = New System.Drawing.Size(800, 415)
+        Me.dgvMostUsed.TabIndex = 1
+        '
+        'btnClose
+        '
+        Me.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.btnClose.Location = New System.Drawing.Point(737, 518)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 6
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'gbFilterOptions
+        '
+        Me.gbFilterOptions.Controls.Add(Me.cbYear)
+        Me.gbFilterOptions.Controls.Add(Me.lblYear)
+        Me.gbFilterOptions.Controls.Add(Me.cbCategoriesPayees)
+        Me.gbFilterOptions.Controls.Add(Me.lblFilterCategoriesPayees)
+        Me.gbFilterOptions.ForeColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.gbFilterOptions.Location = New System.Drawing.Point(12, 12)
+        Me.gbFilterOptions.Name = "gbFilterOptions"
+        Me.gbFilterOptions.Size = New System.Drawing.Size(800, 79)
+        Me.gbFilterOptions.TabIndex = 7
+        Me.gbFilterOptions.TabStop = False
+        Me.gbFilterOptions.Text = "Filter Options"
+        '
+        'cbYear
+        '
+        Me.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbYear.FormattingEnabled = True
+        Me.cbYear.Location = New System.Drawing.Point(25, 37)
+        Me.cbYear.Name = "cbYear"
+        Me.cbYear.Size = New System.Drawing.Size(121, 21)
+        Me.cbYear.TabIndex = 2
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.ForeColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.lblYear.Location = New System.Drawing.Point(25, 21)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(29, 13)
+        Me.lblYear.TabIndex = 1
+        Me.lblYear.Text = "Year"
+        '
+        'cbCategoriesPayees
+        '
+        Me.cbCategoriesPayees.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbCategoriesPayees.FormattingEnabled = True
+        Me.cbCategoriesPayees.Items.AddRange(New Object() {"Categories", "Payees"})
+        Me.cbCategoriesPayees.Location = New System.Drawing.Point(152, 37)
+        Me.cbCategoriesPayees.Name = "cbCategoriesPayees"
+        Me.cbCategoriesPayees.Size = New System.Drawing.Size(182, 21)
+        Me.cbCategoriesPayees.TabIndex = 4
+        '
+        'lblFilterCategoriesPayees
+        '
+        Me.lblFilterCategoriesPayees.AutoSize = True
+        Me.lblFilterCategoriesPayees.ForeColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(66, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.lblFilterCategoriesPayees.Location = New System.Drawing.Point(152, 21)
+        Me.lblFilterCategoriesPayees.Name = "lblFilterCategoriesPayees"
+        Me.lblFilterCategoriesPayees.Size = New System.Drawing.Size(107, 13)
+        Me.lblFilterCategoriesPayees.TabIndex = 3
+        Me.lblFilterCategoriesPayees.Text = "Categories or Payees"
+        '
+        'frmMostUsedCategoriesPayees
+        '
+        Me.AcceptButton = Me.btnClose
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(251, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(824, 553)
+        Me.Controls.Add(Me.gbFilterOptions)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.dgvMostUsed)
+        Me.Name = "frmMostUsedCategoriesPayees"
+        Me.Text = "frmMostUsedCategoriesPayees"
+        CType(Me.dgvMostUsed, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbFilterOptions.ResumeLayout(False)
+        Me.gbFilterOptions.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvMostUsed As DataGridView
+    Friend WithEvents btnClose As Button
+    Friend WithEvents gbFilterOptions As GroupBox
+    Friend WithEvents cbYear As ComboBox
+    Friend WithEvents lblYear As Label
+    Friend WithEvents cbCategoriesPayees As ComboBox
+    Friend WithEvents lblFilterCategoriesPayees As Label
+End Class
---
+++ Checkbook/frmMostUsedCategoriesPayees.vb
@@ -0,0 +1,103 @@
+﻿Public Class frmMostUsedCategoriesPayees
+
+    Private yearList As New List(Of Integer)
+
+    Private Sub frmMostUsedCategoriesPayees_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        AddColumns()
+        DetermineYearsInLedger()
+        cbCategoriesPayees.Text = "Categories"
+        cbYear.SelectedIndex = cbYear.FindStringExact(yearList.Max.ToString) 'SELECTS THE MOST RECENT YEAR FROM YEAR LIST. THIS
+
+    End Sub
+
+    Private Sub AddColumns()
+
+        Dim colNumberOfTransations As New DataGridViewTextBoxColumn
+        Dim colItem As New DataGridViewTextBoxColumn
+        Dim colTotalPayments As New DataGridViewTextBoxColumn
+        Dim colTotalDeposits As New DataGridViewTextBoxColumn
+        Dim colAveragePerMonth As New DataGridViewTextBoxColumn
+        Dim colAveragePerYear As New DataGridViewTextBoxColumn
+
+        colItem.CellTemplate = New DataGridViewTextBoxCell
+        colItem.Name = "category"
+        colItem.HeaderText = "Category"
+        colItem.ReadOnly = True
+
+        colTotalPayments.CellTemplate = New DataGridViewTextBoxCell
+        colTotalPayments.Name = "payments"
+        colTotalPayments.HeaderText = "Payments"
+        colTotalPayments.ReadOnly = True
+
+        colTotalDeposits.CellTemplate = New DataGridViewTextBoxCell
+        colTotalDeposits.Name = "deposits"
+        colTotalDeposits.HeaderText = "Deposits"
+        colTotalDeposits.ReadOnly = True
+
+        colAveragePerMonth.CellTemplate = New DataGridViewTextBoxCell
+        colAveragePerMonth.Name = "averagePerMonth"
+        colAveragePerMonth.HeaderText = "Ave. Monthly"
+        colAveragePerMonth.ReadOnly = True
+
+        colAveragePerYear.CellTemplate = New DataGridViewTextBoxCell
+        colAveragePerYear.Name = "averagePerYear"
+        colAveragePerYear.HeaderText = "Ave. Yearly"
+        colAveragePerYear.ReadOnly = True
+
+        colNumberOfTransations.CellTemplate = New DataGridViewTextBoxCell
+        colNumberOfTransations.Name = "numberOfTransactions"
+        colNumberOfTransations.HeaderText = "# Transactions"
+        colNumberOfTransations.ReadOnly = True
+
+        dgvMostUsed.Columns.Add(colItem)
+        dgvMostUsed.Columns.Add(colTotalPayments)
+        dgvMostUsed.Columns.Add(colTotalDeposits)
+        dgvMostUsed.Columns.Add(colAveragePerMonth)
+        dgvMostUsed.Columns.Add(colAveragePerYear)
+        dgvMostUsed.Columns.Add(colNumberOfTransations)
+
+    End Sub
+
+    Private Sub AddRow(ByVal _category As String, ByVal _budget As String)
+
+        dgvMostUsed.Rows.Add(_category, _budget)
+        dgvMostUsed.ClearSelection()
+
+    End Sub
+
+    Private Sub cbYear_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbYear.SelectedIndexChanged
+
+
+
+    End Sub
+
+    Private Sub DetermineYearsInLedger()
+
+        For Each dgvRow As DataGridViewRow In MainForm.dgvLedger.Rows 'FINDS ALL THE YEARS THAT EXIST IN THE LEDGER AND LOADS THEM INTO THE LIST
+
+            Dim intYear As Integer
+            Dim i As Integer = Nothing
+            Dim dtDate As Date
+            i = dgvRow.Index
+
+            dtDate = MainForm.dgvLedger.Item("TransDate", i).Value
+            intYear = dtDate.Year
+
+            If Not yearList.Contains(intYear) Then
+
+                yearList.Add(intYear)
+
+            End If
+
+            If Not cbYear.Items.Contains(intYear) Then
+
+                cbYear.Items.Add(intYear) 'IF THE YEAR DOESNT ALREADY EXIST WITHIN THE LIST THEN IT WILL BE ADDED
+
+            End If
+
+        Next
+
+    End Sub
+
+End Class
